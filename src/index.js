755e0afa4f005d57f2285cec3daa549c6e9600ea
NicholasGalarza <nicogala93@gmail.com>
2017-10-03 12:20:28 -0400

    added map

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-console.log("Hello from JavaScript");
+const mapboxgl = require("mapbox-gl");
 
+mapboxgl.accessToken = "pk.eyJ1IjoicGluZ3dpbnpsb3R5IiwiYSI6ImNqOGJ0NGF4ejAxcnQyd29razNnM29ncDYifQ.9hm_D0gtkCHlJSEm6rkomg";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack coordinates
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
